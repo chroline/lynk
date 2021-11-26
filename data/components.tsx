891efa72318460c6b166935ff0f2ca5cd82145c2
Cole Gawin <colegawin@gmail.com>
2021-11-26 13:54:06 -0600

    ✨(content) add more socials

--- data/components.tsx
+++ data/components.tsx
@@ -2,7 +2,7 @@ import { AiFillInstagram } from "react-icons/ai";
 import { FaSnapchatGhost } from "react-icons/fa";
 import { HiGlobeAlt, HiViewGrid, HiBookOpen } from "react-icons/hi";
 import { MdAttachMoney } from "react-icons/md";
-import { RiGithubFill, RiLinkedinBoxFill } from "react-icons/ri";
+import { RiGithubFill, RiLinkedinBoxFill, RiTwitterFill, RiAtFill } from "react-icons/ri";
 
 export default {
   HiGlobeAlt,
@@ -13,4 +13,6 @@ export default {
   AiFillInstagram,
   FaSnapchatGhost,
   RiLinkedinBoxFill,
+  RiTwitterFill,
+  RiAtFill,
 };
